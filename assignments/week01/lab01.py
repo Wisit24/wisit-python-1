fa7da9aa4245d711bf6f71070c83719bf8c4ebc6
wisit Pythone 1 <wiset.m@ku.th>
2025-07-08 14:58:25 +0700

    lab01

---
+++ assignments/week01/lab01.py
@@ -0,0 +1,24 @@
+# Exercise 1: Personal Profile Creator
+print("=== Personal Profile Creator ===")
+# Create a program that asks for:
+# - Full name
+# - Age
+# - Email
+# - Phone number
+# - Favorite hobby
+# Then display it as a profile
+
+# Write your solution here:
+
+full_name = input("what is your name? :")
+age = input ("How old are you? :")
+emaill = input ("What is your emill? :")
+phone = input ("What is your phone number? :")
+hobby = input ("What is your favorite hobby? :")
+
+print("== Your profile ==")
+print("Full name: " + full_name)
+print("Age: ",age)
+print("Email: ",email)
+print("Phone no.: ",phone_number)
+print("Faverite hobby: ",hobby)
